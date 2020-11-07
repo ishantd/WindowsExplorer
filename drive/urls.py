05760092d9bacb1de4d8317131573eadeaf8fc45
Ishant Dahiya <ishantdahiya@gmail.com>
2020-11-08 03:14:29 +0530

    communication to django server

--- drive/urls.py
+++ drive/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # api endpoints for file watcher
+    path('created/', views.created),
 ]
--- drive/watcher.py
+++ drive/watcher.py
@@ -3,11 +3,17 @@ import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+import os
+import requests
+
+os.environ['NO_PROXY'] = '127.0.0.1'
 
 # Event logger
 class Event(LoggingEventHandler):
     def on_created(self, event):
         print("created", event)
+        r = requests.get('http://127.0.0.1:8000/created/')
+        print(r.content)
     def on_deleted(self, event):
         print("Deleted", event)
     def on_modified(self, event):
@@ -19,7 +25,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = '/home/ishant/Downloads'
     print(path)
     event_handler = Event()
     observer = Observer()
