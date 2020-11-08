ad6e401f32a2f620740de09223ddaff983ec5f8e
Ishant Dahiya <ishantdahiya@gmail.com>
2020-11-08 18:22:29 +0530

    all api setup

--- drive/views.py
+++ drive/views.py
@@ -77,40 +77,55 @@ def modified(request):
             path = re.findall(r'"([^"]*)"', event)[0]
             file = File.objects.get(path=path)
             file.save()
+        if 'DirModifiedEvent' in event:
+            event = event.replace("'", '"')
+            path = re.findall(r'"([^"]*)"', event)[0]
+            directory = Directory.objects.get(path=path)
+            directory.save()
     return HttpResponse(status=200)
 
-# @csrf_exempt
-# def moved(request):
-#     if request.method == 'POST':
-#         event = request.POST.get('event')
-#         if 'FileCreatedEvent' in event:
-#             event = event.replace("'", '"')
-#             path = re.findall(r'"([^"]*)"', event)[0]
-#             name = path.split('/')[-1]
-#             extension = name.split('.')[-1]
-#             parent_path = path.replace(f'/{name}', "")
-#             parent = Directory.objects.get(path=parent_path)
-#             data = {
-#                 'parent': parent,
-#                 'name': name,
-#                 'path': path,
-#                 'extension': extension,
-#             }
-#             file, created = File.objects.get_or_create(**data)
-#             if created:
-#                 print("File created in database")
-#         if 'DirCreatedEvent' in event:
-#             event = event.replace("'", '"')
-#             path = re.findall(r'"([^"]*)"', event)[0]
-#             name = path.split('/')[-1]
-#             parent_path = path.replace(f'/{name}', "")
-#             parent = Directory.objects.get(path=parent_path)
-#             data = {
-#                 'parent': parent,
-#                 'name': name,
-#                 'path': path,
-#             }
-#             directory, created = Directory.objects.get_or_create(**data)
-#             if created:
-#                 print("Directory created in database")
-#     return HttpResponse(status=200)
+@csrf_exempt
+def moved(request):
+    if request.method == 'POST':
+        event = request.POST.get('event')
+        if 'FileMovedEvent' in event:
+            event = event.replace("'", '"')
+            paths = re.findall(r'"([^"]*)"', event)
+            old_path, new_path = paths
+            if old_path.split('/')[-1] == new_path.split('/')[-1]:
+                # print("Moved Element")
+                name = old_path.split('/')[-1]
+                file = File.objects.get(path=old_path)
+                file.path = new_path
+                parent_path = new_path.replace(f'/{name}', "")
+                parent = Directory.objects.get(path=parent_path)
+                file.parent = parent
+                file.save()
+            else:
+                # print("Element Renamed")
+                new_name = new_path.split('/')[-1]
+                file = File.objects.get(path=old_path)
+                file.path = new_path
+                file.name = new_name
+                file.save()
+        if 'DirMovedEvent' in event:
+            event = event.replace("'", '"')
+            paths = re.findall(r'"([^"]*)"', event)
+            old_path, new_path = paths
+            if old_path.split('/')[-1] == new_path.split('/')[-1]:
+                # print("Moved Element")
+                name = old_path.split('/')[-1]
+                directory = Directory.objects.get(path=old_path)
+                directory.path = new_path
+                parent_path = new_path.replace(f'/{name}', "")
+                parent = Directory.objects.get(path=parent_path)
+                directory.parent = parent
+                directory.save()
+            else:
+                # print("Element Renamed")
+                new_name = new_path.split('/')[-1]
+                directory = Directory.objects.get(path=old_path)
+                directory.path = new_path
+                directory.name = new_name
+                directory.save()
+    return HttpResponse(status=200)
